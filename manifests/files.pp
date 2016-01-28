3fc4f96ba57aba9ae4a5e7968dcb9109a6526ed9
Smit Jain <smitjain.sj@gmail.com>
2016-01-28 17:57:17 +0530

    Need to update the Git clone step and also the DB with all config place to put in

--- manifests/files.pp
+++ manifests/files.pp
@@ -11,26 +11,13 @@ File {
 
 
 file {  
-	'/etc/icingaweb2/resources.ini' :
-	content => template('icinga2/resources.erb'),
+	 '/etc/icingaweb2/resources.ini':
+	content => template('icinga2/resources.erb');
 
-	'/etc/icingaweb2/config.ini' :
-	content => template('icinga2/config.erb '),
-
-	'/etc/icingaweb2/authentication.ini':
-	content => template('icinga2/authentication.erb'),
-
-	'/etc/icingaweb2/modules/monitoring/config.ini':
-	content => template('icinga2/monitor_config.erb'),
-
-	'/etc/icingaweb2/modules/monitoring/backends.ini'
-	content => template('icinga2/backends.erb'),
-
-	'/etc/icingaweb2/modules/monitoring/commandtransports.ini'
-	content => template('icinga2/commandtransports.erb') ,
-
-	require => Class['icinga2::icinga2web'],
+	require => Class[icinga2::icinga2web],
 	notify => Service[$icinga2_service],
-}
+
+	}
 	
 }
+
